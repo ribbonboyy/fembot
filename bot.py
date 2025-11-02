c39cf17ca0b65513796538bf2503912239927e04
Madeline <166247241+ribbonboyy@users.noreply.github.com>
2025-11-02 19:09:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 import random
 from discord.ext import commands
+import os
+bot.run(os.getenv("DISCORD_TOKEN"))
+
 
 intents = discord.Intents.default()
 intents.message_content = True
